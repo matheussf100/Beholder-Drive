621230112181b0749ae3b69479afce8e1b338393
matheussf100 <matheussf300@gmail.com>
2023-01-11 13:57:31 -0300

    Form (Login + Submit) e useState

---
+++ frontend/src/services/AuthService.js
@@ -0,0 +1,11 @@
+export function doLogin(email, password){
+    if (email === 'Jooj@hotmail.com'
+    && password === '123456') {
+        return true;
+    }
+    return false;
+}
+
+export function doLogout(){
+
+}
